f87c77e527233fd72a2616b1c7446bcfed4643a1
Howtoon <Howtoon@Williams-MacBook-Pro.local>
2016-11-08 11:25:34 -0600

    created Permit object

---
+++ Permit.java
@@ -0,0 +1,11 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Permit {
+	private String id;
+	private String type;
+	DateFormat formatter = new SimpleDateFormat("MM/dd/yy");
+	Date expirationDate = formatter.parse("01/29/02");
+	
+}
