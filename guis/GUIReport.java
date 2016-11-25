7f927af457ba95270f11926ab3e7baa7d759db40
Nathan Cooper <nac33@students.uwf.edu>
2016-11-25 09:47:21 -0600

    Fix image resizing issue

--- guis/GUIReport.java
+++ guis/GUIReport.java
@@ -1,7 +1,6 @@
 package guis;
 
-import java.awt.Graphics2D;
-import java.awt.RenderingHints;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
@@ -14,124 +13,135 @@ import objects.ParkingLot;
 public class GUIReport extends JPanel
 {
 
-	private Controller controller;
-	private ParkingLot lot;
-	private JLabel lotImage;
-   private String parkingLotID;
-	
+    private Controller controller;
+    private ParkingLot lot;
+    private JLabel lotImage;
+    private String parkingLotID;
 
-	public GUIReport(Controller controller)
-	{
 
-		this.controller = controller;
-		this.lotImage = new JLabel("");
-		this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
-		this.addComponents();
-		this.add(lotImage);
+    public GUIReport(Controller controller)
+    {
 
-	}
+        this.controller = controller;
+        this.lotImage = new JLabel("");
+        this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+        this.addComponents();
+        this.add(lotImage);
 
-	private void addComponents()
-	{
+    }
 
-		JPanel componentPanel = new JPanel();
+    private void addComponents()
+    {
 
-		JLabel lotID = new JLabel("Lot ID:");
-		JTextField lotField = new JTextField(20);
-      parkingLotID = lotField.getText();
+        JPanel componentPanel = new JPanel();
 
-		JButton btnViewLot = new JButton("View Parking Lot");
-		btnViewLot.addActionListener(new ActionListener()
-		{
-			public void actionPerformed(ActionEvent e)
-			{
-				lot = controller.getLot(lotField.getText());
-				if(lot == null)
-					controller.displayError("There is no lot with that ID");
-				else if(lotField.getText().equals(""))
-					controller.displayError("There is not a lot specified");
-				else
-               addStatistics();
-					//drawParkingLot(lot.getLotID().toLowerCase());
-			}
-		});
+        JLabel lotID = new JLabel("Lot ID:");
+        JTextField lotField = new JTextField(20);
+        parkingLotID = lotField.getText();
 
-		componentPanel.add(lotID);
-		componentPanel.add(lotField);
-		componentPanel.add(btnViewLot);
-		
-		JButton btnGoBack = new JButton("Go Back");
-		btnGoBack.addActionListener(new ActionListener()
-		{
-			public void actionPerformed(ActionEvent e)
-			{
-				controller.displayGUIMainMenu();
-			}
-		});
-		
-		this.add(componentPanel);
-		this.add(btnGoBack);
-
-	}
-	
-	private void addStatistics()
-	{
-	   JPanel componentPanel = new JPanel();
-
-		JLabel spaceAvail = new JLabel("Enter # of spaces available: ");
-		JTextField numSpaces = new JTextField(5);
-      
-		JButton btnAddStats = new JButton("Add Statistics");
-		btnAddStats.addActionListener(new ActionListener()
-		{
-			public void actionPerformed(ActionEvent e)
-			{
-            int space = Integer.parseInt(numSpaces.getText());
-            ParkingLot result = controller.updateLotCars(parkingLotID, space);
-            if (result != null)
+        JButton btnViewLot = new JButton("View Parking Lot");
+        btnViewLot.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
             {
-               controller.displayError("Report was successfully submitted");
+                lot = controller.getLot(lotField.getText());
+                if (lot == null)
+                    controller.displayError("There is no lot with that ID");
+                else if (lotField.getText().equals(""))
+                    controller.displayError("There is not a lot specified");
+                else
+                    drawParkingLot(lot.getLotID().toLowerCase());
             }
-            if (result == null)
+        });
+
+        componentPanel.add(lotID);
+        componentPanel.add(lotField);
+        componentPanel.add(btnViewLot);
+
+        JButton btnGoBack = new JButton("Go Back");
+        btnGoBack.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
             {
-               controller.displayError("An error occurred while updating lot information");
+                controller.displayGUIMainMenu();
             }
-			}
-		});
-      
-      componentPanel.add(spaceAvail);
-		componentPanel.add(numSpaces);
-		componentPanel.add(btnAddStats);
-      
-      this.add(componentPanel);
-      this.revalidate();
-	}
-	
-	private void drawParkingLot(String lotID)
-	{
-		BufferedImage bi = null;
-		try
-		{
+        });
 
-			ImageIcon imageicon = new ImageIcon(String.format("resources/%s_lot.png", lotID));
-			
-			bi = new BufferedImage(imageicon.getIconWidth(), imageicon.getIconHeight(), BufferedImage.TYPE_INT_RGB);
-			Graphics2D g2d = (Graphics2D) bi.createGraphics();
-			g2d.addRenderingHints(new RenderingHints(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY));
-			g2d.drawImage(imageicon.getImage(), 0, 0, this.getWidth(), this.getHeight(), this);
+        this.add(componentPanel);
+        this.add(btnGoBack);
 
-		}
-		catch(Exception e)
-		{
+    }
 
-			System.out.println("Here is the lot id: " + lotID);
-			e.printStackTrace();
-		}
+    private void addStatistics()
+    {
+        JPanel componentPanel = new JPanel();
 
-		lotImage.setIcon(new ImageIcon(bi));
-		
-		this.revalidate();
-	}
-	
+        JLabel spaceAvail = new JLabel("Enter # of spaces available: ");
+        JTextField numSpaces = new JTextField(5);
+
+        JButton btnAddStats = new JButton("Add Statistics");
+        btnAddStats.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                int space = Integer.parseInt(numSpaces.getText());
+                ParkingLot result = controller.updateLotCars(parkingLotID, space);
+                if (result != null)
+                {
+                    controller.displayError("Report was successfully submitted");
+                }
+                if (result == null)
+                {
+                    controller.displayError("An error occurred while updating lot information");
+                }
+            }
+        });
+
+        componentPanel.add(spaceAvail);
+        componentPanel.add(numSpaces);
+        componentPanel.add(btnAddStats);
+
+        this.add(componentPanel);
+        this.revalidate();
+    }
+
+    private void drawParkingLot(String lotID)
+    {
+        BufferedImage bi = null;
+        try
+        {
+
+            ImageIcon imageicon = new ImageIcon(String.format("resources/%s_lot.png", lotID));
+
+            bi = new BufferedImage(imageicon.getIconWidth(), imageicon.getIconHeight(), BufferedImage.TYPE_INT_RGB);
+            Graphics2D g2d = (Graphics2D) bi.createGraphics();
+            g2d.addRenderingHints(new RenderingHints(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY));
+            g2d.drawImage(imageicon.getImage(), 0, 0, this.getWidth(), this.getHeight(), this);
+
+        }
+        catch (Exception e)
+        {
+
+            System.out.println("Here is the lot id: " + lotID);
+            e.printStackTrace();
+        }
+
+        lotImage.setText(lotID);
+        lotImage.setIcon(new ImageIcon(bi));
+
+        this.revalidate();
+    }
+
+    public void paintComponent(Graphics g)
+    {
+
+//        System.out.println("Reload");
+        if (lotImage.getIcon() != null)
+        {
+            drawParkingLot(lotImage.getText());
+        }
+
+    }
+
 
 }
