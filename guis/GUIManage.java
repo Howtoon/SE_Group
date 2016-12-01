04e913924402e1e77c60123d57f9c8151f729801
Howtoon <Howtoon@Williams-MacBook-Pro.local>
2016-12-01 14:34:51 -0600

    Fixed bug in GUIRestric that was not updateing object before redisplay after open/closed toggle

--- guis/GUIManage.java
+++ guis/GUIManage.java
@@ -10,133 +10,136 @@ import database.*;
 public class GUIManage extends JPanel
 {
 
-    private JTextField username;
-	private Controller controller;
-	private User user;
-	private JFrame frame;
-	private JPanel userDisplayPanel;
-	private JPanel changeBtnPanel;
+   private JTextField username;
+   private Controller controller;
+   private User user;
+   private JFrame frame;
+   private JPanel userDisplayPanel;
+   private JPanel changeBtnPanel;
 
 
-	public GUIManage(Controller controller, JFrame frame)
-	{
-		
-		this.frame = frame;
-		this.controller = controller;
-		this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
-		this.addFields();
-		this.addButtons();
-		this.userDisplayPanel = new JPanel();
-		this.changeBtnPanel = new JPanel();
+   public GUIManage(Controller controller, JFrame frame)
+   {
+   	
+      this.frame = frame;
+      this.controller = controller;
+      this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+      this.addFields();
+      this.addButtons();
+      this.userDisplayPanel = new JPanel();
+      this.changeBtnPanel = new JPanel();
       this.setBorder(BorderFactory.createLineBorder(Color.black));
+   
+   }
 
-	}
-
-	public void addButtons()
-	{
-		JPanel btnPanel = new JPanel();
-		btnPanel.setLayout(new FlowLayout());
+   public void addButtons()
+   {
+      JPanel btnPanel = new JPanel();
+      btnPanel.setLayout(new FlowLayout());
       btnPanel.setBackground(Color.CYAN);
-		JButton btnLogin = new JButton("Search for User");
-		btnLogin.addActionListener(new ActionListener()
-		{
-			public void actionPerformed(ActionEvent e)
-			{
-				user = controller.getUser(username.getText());
-
-				if (user == null)
-				{
-					controller.displayError("User Does Not Exist.");
-				}
-				else
-				{
-					userDisplayPanel.removeAll();
-					changeBtnPanel.removeAll();
-					frame.revalidate();
-					displayUser();
-					displayModBtn();
-					System.out.println("Display User");
-				}
-			}
-		});
-		btnPanel.add(btnLogin);
-		JButton btnGoBack = new JButton("Go Back");
-		btnGoBack.addActionListener(new ActionListener()
-		{
-			public void actionPerformed(ActionEvent e)
-			{
-				controller.displayGUIMainMenu();
-			}
-		});
-		btnPanel.add(btnGoBack);
+      JButton btnLogin = new JButton("Search for User");
+      btnLogin.addActionListener(
+         new ActionListener()
+         {
+            public void actionPerformed(ActionEvent e)
+            {
+               user = controller.getUser(username.getText());
+            
+               if (user == null)
+               {
+                  controller.displayError("User Does Not Exist.");
+               }
+               else
+               {
+                  userDisplayPanel.removeAll();
+                  changeBtnPanel.removeAll();
+                  frame.revalidate();
+                  displayUser();
+                  displayModBtn();
+                  System.out.println("Display User");
+               }
+            }
+         });
+      btnPanel.add(btnLogin);
+      JButton btnGoBack = new JButton("Go Back");
+      btnGoBack.addActionListener(
+         new ActionListener()
+         {
+            public void actionPerformed(ActionEvent e)
+            {
+               controller.displayGUIMainMenu();
+            }
+         });
+      btnPanel.add(btnGoBack);
       btnPanel.setBorder(BorderFactory.createLineBorder(Color.black));
-		this.add(btnPanel);
-	}
+      this.add(btnPanel);
+   }
 
-	public void addFields()
-	{
-		JPanel userPanel = new JPanel();
-		userPanel.setLayout(new FlowLayout());
+   public void addFields()
+   {
+      JPanel userPanel = new JPanel();
+      userPanel.setLayout(new FlowLayout());
       JLabel userLabel = new JLabel("Enter User Name: ");
-		username = new JTextField(20);
-		userPanel.add(userLabel);
-		userPanel.add(username);
+      username = new JTextField(20);
+      userPanel.add(userLabel);
+      userPanel.add(username);
       userPanel.setBackground(Color.CYAN);
       userPanel.setBorder(BorderFactory.createLineBorder(Color.black));
-		this.add(userPanel);
+      this.add(userPanel);
+   
+   }
 
-	}
-
-	public void displayUser()
-	{
-		JLabel userName = new JLabel(user.getName());
-		JLabel userPermissions = new JLabel(user.getPermissions().getpString());
-		userDisplayPanel.add(userName);
-		userDisplayPanel.add(userPermissions);
+   public void displayUser()
+   {
+      JLabel userName = new JLabel(user.getName());
+      JLabel userPermissions = new JLabel(user.getPermissions().getpString());
+      userDisplayPanel.add(userName);
+      userDisplayPanel.add(userPermissions);
       userDisplayPanel.setBorder(BorderFactory.createLineBorder(Color.black));
-		this.add(userDisplayPanel);
-		frame.revalidate();
-	}
+      this.add(userDisplayPanel);
+      frame.revalidate();
+   }
 
-	public void displayModBtn()
-	{  
-		JButton changePermission = new JButton("Toggle User Permission");
-		changePermission.addActionListener(new ActionListener()
-		{
-			public void actionPerformed(ActionEvent e)
-			{
-				if (user.getPermissions().getpString() == "USER")
-				{
-					controller.updatePermissions (user.getName(), 1);
-				}
-				else if (user.getPermissions().getpString() == "ADMIN")
-				{
-					controller.updatePermissions (user.getName(), 0);
-				}
-				else if (user.getPermissions().getpString() == "SUPERVISOR")
-				{
-					controller.displayError("You Cannot Change The Supervisor's Permissions!!!");
-				}
-				user = controller.getUser(username.getText());
-				if (user == null)
-				{
-					controller.displayError("User Does Not Exist.");
-				}
-				else
-				{
-					userDisplayPanel.removeAll();
-					changeBtnPanel.removeAll();
-					frame.revalidate();
-					displayUser();
-					displayModBtn();
-					System.out.println("Display User");
-				}
-			}
-		});
+   public void displayModBtn()
+   {  
+      JButton changePermission = new JButton("Toggle User Permission");
+      changePermission.addActionListener(
+         new ActionListener()
+         {
+            public void actionPerformed(ActionEvent e)
+            {
+               if (user.getPermissions().getpString() == "USER")
+               {
+                  controller.updatePermissions (user.getName(), 1);
+               }
+               else if (user.getPermissions().getpString() == "ADMIN")
+               {
+                  controller.updatePermissions (user.getName(), 0);
+               }
+               else if (user.getPermissions().getpString() == "SUPERVISOR")
+               {
+                  controller.displayError("You Cannot Change The Supervisor's Permissions!!!");
+               }
+               user = controller.getUser(username.getText());
+               if (user == null)
+               {
+                  controller.displayError("User Does Not Exist.");
+               }
+               else
+               {
+                  userDisplayPanel.removeAll();
+                  changeBtnPanel.removeAll();
+                  frame.revalidate();
+                  displayUser();
+                  displayModBtn();
+                  System.out.println("Display User");
+               }
+            }
+         });
       changeBtnPanel.setBackground(Color.ORANGE);
-		changeBtnPanel.add(changePermission);
+      changeBtnPanel.add(changePermission);
       changeBtnPanel.setBorder(BorderFactory.createLineBorder(Color.black));
-		this.add(changeBtnPanel);
-		frame.revalidate();
-	}
+      this.add(changeBtnPanel);
+      frame.revalidate();
+   }
 }
--- guis/GUIRestric.java
+++ guis/GUIRestric.java
@@ -12,140 +12,145 @@ public class GUIRestric extends JPanel
 {
 
    private JTextField lotName;
-	private Controller controller;
-	private ParkingLot lot;
-	private JFrame frame;
-	private JPanel lotDisplayPanel;
-	private JPanel changeBtnPanel;
+   private Controller controller;
+   private ParkingLot lot;
+   private JFrame frame;
+   private JPanel lotDisplayPanel;
+   private JPanel changeBtnPanel;
 
 
-	public GUIRestric(Controller controller, JFrame frame)
-	{
-		
-		this.frame = frame;
-		this.controller = controller;
-		this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
-		this.addFields();                                                 //add search field
-		this.addButtons();                                                //add search and go-back buttons
-		this.lotDisplayPanel = new JPanel();                              //create global var for panel to display lot info
-		this.changeBtnPanel = new JPanel();                               //create global var for panel for toggle button
+   public GUIRestric(Controller controller, JFrame frame)
+   {
+   	
+      this.frame = frame;
+      this.controller = controller;
+      this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+      this.addFields();                                                 //add search field
+      this.addButtons();                                                //add search and go-back buttons
+      this.lotDisplayPanel = new JPanel();                              //create global var for panel to display lot info
+      this.changeBtnPanel = new JPanel();                               //create global var for panel for toggle button
       this.setBorder(BorderFactory.createLineBorder(Color.black));
+   
+   }
 
-	}
-
-	public void addButtons()
-	{
-		JPanel btnPanel = new JPanel();
+   public void addButtons()
+   {
+      JPanel btnPanel = new JPanel();
       btnPanel.setBorder(BorderFactory.createLineBorder(Color.black));
-		btnPanel.setLayout(new FlowLayout());
+      btnPanel.setLayout(new FlowLayout());
       btnPanel.setBackground(Color.CYAN);
-		JButton btnLookUp = new JButton("Search for Parking Lot");
-		btnLookUp.addActionListener(new ActionListener()
-		{
-			public void actionPerformed(ActionEvent e)
-			{
-				lot = controller.getLot(lotName.getText());        //Look up Lot by LotID and assign ParkingLot object to var lot
-				if (lot == null)
-				{
-					controller.displayError("Parking Lot Does Not Exist.");
-				}
-				else
-				{
-					lotDisplayPanel.removeAll();           //Remove all previous lot info
-					changeBtnPanel.removeAll();            //Remove all toggle buttons
-					frame.revalidate();                    //revalidate to ensure everything cleared
-					displayLot();                          //redisplay lot info after change has been made
-					displayModBtn();                       //reload toggle buttons
-					System.out.println("Display Parking Lot");   
-				}
-			}
-		});
-		btnPanel.add(btnLookUp);                        //Add search buttons
-		JButton btnGoBack = new JButton("Go Back");     //returns user to GUIMainMenu()
-		btnGoBack.addActionListener(new ActionListener()
-		{
-			public void actionPerformed(ActionEvent e)
-			{
-				controller.displayGUIMainMenu();
-			}
-		});
-		btnPanel.add(btnGoBack);
-		this.add(btnPanel);
-	}
+      JButton btnLookUp = new JButton("Search for Parking Lot");
+      btnLookUp.addActionListener(
+         new ActionListener()
+         {
+            public void actionPerformed(ActionEvent e)
+            {
+               lot = controller.getLot(lotName.getText());        //Look up Lot by LotID and assign ParkingLot object to var lot
+               if (lot == null)
+               {
+                  controller.displayError("Parking Lot Does Not Exist.");
+               }
+               else
+               {
+                  lotDisplayPanel.removeAll();           //Remove all previous lot info
+                  changeBtnPanel.removeAll();            //Remove all toggle buttons
+                  frame.revalidate();                    //revalidate to ensure everything cleared
+                  displayLot();                          //redisplay lot info after change has been made
+                  displayModBtn();                       //reload toggle buttons
+                  System.out.println("Display Parking Lot");   
+               }
+            }
+         });
+      btnPanel.add(btnLookUp);                        //Add search buttons
+      JButton btnGoBack = new JButton("Go Back");     //returns user to GUIMainMenu()
+      btnGoBack.addActionListener(
+         new ActionListener()
+         {
+            public void actionPerformed(ActionEvent e)
+            {
+               controller.displayGUIMainMenu();
+            }
+         });
+      btnPanel.add(btnGoBack);
+      this.add(btnPanel);
+   }
 
-	public void addFields()
-	{
-		JPanel fieldPanel = new JPanel();                           //search field panel
+   public void addFields()
+   {
+      JPanel fieldPanel = new JPanel();                           //search field panel
       fieldPanel.setBorder(BorderFactory.createLineBorder(Color.black));
-		fieldPanel.setLayout(new FlowLayout());
+      fieldPanel.setLayout(new FlowLayout());
       JLabel lotLabel = new JLabel("Enter Parking Lot ID: ");     
-		lotName = new JTextField(20);
-		fieldPanel.add(lotLabel);
-		fieldPanel.add(lotName);
+      lotName = new JTextField(20);
+      fieldPanel.add(lotLabel);
+      fieldPanel.add(lotName);
       fieldPanel.setBackground(Color.CYAN);
+   
+      this.add(fieldPanel);
+   
+   }
 
-		this.add(fieldPanel);
-
-	}
-
-	public void displayLot()
-	{
+   public void displayLot()
+   {
       String restric;
-		JLabel nameLabel = new JLabel(lotName.getText());
-		if (lot.isOpen() == true)
+      JLabel nameLabel = new JLabel(lotName.getText());
+      if (lot.isOpen() == true)
       {
          restric = "Open";
+         System.out.println("JLabel should say open");
       }
       else
       {
          restric = "Closed";
+         System.out.println("JLabel should say closed");
       }
       JLabel lotRestriction = new JLabel(restric);
-		lotDisplayPanel.add(nameLabel);
-		lotDisplayPanel.add(lotRestriction);
+      lotDisplayPanel.add(nameLabel);
+      lotDisplayPanel.add(lotRestriction);
+      System.out.println("Updated Open/Closed JLabel");
+      this.add(lotDisplayPanel);
+      frame.revalidate();
+   }
 
-		this.add(lotDisplayPanel);
-		frame.revalidate();
-	}
-
-	public void displayModBtn()
-	{  
-		JButton changeRestriction = new JButton("Toggle Lot Permission");
-		changeRestriction.addActionListener(new ActionListener()
-		{
-			public void actionPerformed(ActionEvent e)
-			{
-				if (lot.isOpen() == true)
-				{
-					controller.setOpen(false);
-               controller.updateLotStatus(lot.getLotID(),false);
-				}
-				else if (lot.isOpen() == false)
-				{
-					controller.setOpen(true);
-               controller.updateLotStatus(lot.getLotID(),true);
-				}
-				String lotNm = lot.getLotID();
-				if (lot == null)
-				{
-					controller.displayError("Lot Does Not Exist.");
-				}
-				else
-				{
-					lotDisplayPanel.removeAll();
-					changeBtnPanel.removeAll();
-					frame.revalidate();
-					displayLot();
-					displayModBtn();
-               frame.revalidate();
-					System.out.println("Display Lot");
-				}
-			}
-		});
+   public void displayModBtn()
+   {  
+      JButton changeRestriction = new JButton("Toggle Lot Permission");
+      changeRestriction.addActionListener(
+         new ActionListener()
+         {
+            public void actionPerformed(ActionEvent e)
+            {
+               if (lot.isOpen() == true)
+               {
+                  controller.setOpen(false);
+                  controller.updateLotStatus(lot.getLotID(),false);
+               }
+               else if (lot.isOpen() == false)
+               {
+                  controller.setOpen(true);
+                  controller.updateLotStatus(lot.getLotID(),true);
+               }
+               String lotNm = lot.getLotID();
+               if (lot == null)
+               {
+                  controller.displayError("Lot Does Not Exist.");
+               }
+               else
+               {
+                  lotDisplayPanel.removeAll();
+                  changeBtnPanel.removeAll();
+                  frame.revalidate();
+                  lot = controller.getLot(lotName.getText());
+                  displayLot();
+                  displayModBtn();
+                  System.out.println("Display Lot");
+               }
+            }
+         });
       changeBtnPanel.setBackground(Color.ORANGE);
       changeBtnPanel.setBorder(BorderFactory.createLineBorder(Color.black));
-		changeBtnPanel.add(changeRestriction);
-		this.add(changeBtnPanel);
-		frame.revalidate();
-	}
+      changeBtnPanel.add(changeRestriction);
+      this.add(changeBtnPanel);
+      frame.revalidate();
+   }
 }
